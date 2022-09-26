6baf6402df8eb1c39555f8e4b05ff0ded3e24947
unknown <Admin@PC-RESERVA.grautecgoiana.local>
2022-09-26 11:16:23 -0300

    Tratamento do problema de múltiplas requests ao buscar dados dos filmes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const API_KEY = '0a951f7339926b0d5f3b76d7e60a7dc0'
 const API_LANGUAGE = 'pt-br'
-const LIST_MOVIES = ['tt12801262', 'tt7146812', 'tt5109280', 'tt3521164', 'tt2380307', 'tt2948372', 'tt8097030']
 const BASE_URL_IMAGE = {
     original: 'http://image.tmdb.org/t/p/original',
     small: 'http://image.tmdb.org/t/p/w500'
 }
 
-const moviesList = document.getElementById('movies')
+const movies = []
+const moviesElement = document.getElementById('movies')
 
 function getUrlMovie(movieId) {
     return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${API_LANGUAGE}`
@@ -20,31 +20,31 @@ function changeButtonMenu() {
     navigation.classList.toggle('active')
 }
 
-function setMainMovie(movieId) {
-    fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
-        const title = document.querySelector('.movie_feature h1')
-        const description = document.querySelector('.movie_feature p')
-        const rating = document.querySelector('.rating strong')
-        const info = document.querySelector('.movie_feature span')
-        const appImage = document.querySelector('.app_image img')
-    
-        const yearRelease = data.release_date.split('-')[0]
-        const image = BASE_URL_IMAGE.original.concat(data.backdrop_path)
-    
-        title.innerHTML = data.title
-        description.innerHTML = data.overview
-        rating.innerHTML = data.vote_average
-        info.innerHTML = yearRelease + ' - ' + data.genres[2].name + ' - ' + 'Filme'
-        appImage.setAttribute('src', image)
+function setMainMovie(movie) {
+    const title = document.querySelector('.movie_feature h1')
+    const description = document.querySelector('.movie_feature p')
+    const rating = document.querySelector('.rating strong')
+    const info = document.querySelector('.movie_feature span')
+    const appImage = document.querySelector('.app_image img')
 
-        changeButtonMenu()
-        //app.style.backgroundImage = `linear-gradient(rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
-    })
+    title.innerHTML = movie.title
+    description.innerHTML = movie.overview
+    rating.innerHTML = movie.vote_average
+    info.innerHTML = movie.release + ' - ' + movie.genre + ' - ' + 'Filme'
+    appImage.setAttribute('src', movie.image.original)
+}
+
+function changeMainMovie(movieId) {
+    const movie = movies.find(movie => movie.id === movieId)
+
+    setMainMovie(movie)
+    changeButtonMenu()        
+
 }
 
 function createButtonMovie(movieId) {
     const button = document.createElement('button')
-    button.setAttribute(`onclick`, `setMainMovie('${movieId}')`)
+    button.setAttribute(`onclick`, `changeMainMovie('${movieId}')`)
     button.innerHTML = '<img src="./images/icon-play-button.png">'
 
     return button
@@ -64,28 +64,48 @@ function createImageMovie(movieImage, movieTitle) {
     return divImageMovie
 }
 
-function createMovie(movieId) {
-    fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
-        const movie = document.createElement('li')
-        movie.classList.add('movie')
+function addMovieInList(movie) {
+    const movieElement = document.createElement('li')
+    movieElement.classList.add('movie')
 
-        const genre = `<span>${data.genres[2].name}</span>`
-        const title = `<strong>${data.title}</strong>`
-        const image = BASE_URL_IMAGE.small.concat(data.backdrop_path)
+    const genre = `<span>${movie.genre}</span>`
+    const title = `<strong>${movie.title}</strong>`
 
-        movie.innerHTML = genre + title
-        movie.appendChild(createButtonMovie(movieId))
-        movie.appendChild(createImageMovie(image, data.title))
+    movieElement.innerHTML = genre + title
+    movieElement.appendChild(createButtonMovie(movie.id))
+    movieElement.appendChild(createImageMovie(movie.image.small, movie.title))
 
-        moviesList.appendChild(movie)
+    moviesElement.appendChild(movieElement)     
+}
+
+function loadMovies() {
+    const LIST_MOVIES = ['tt12801262', 'tt7146812', 'tt5109280', 'tt3521164', 'tt2380307', 'tt2948372', 'tt8097030'] 
+    
+    LIST_MOVIES.map((movie, index) => {
+        fetch(getUrlMovie(movie)).then(response => response.json()).then(data => {
+            const movieData = {
+                id: movie,
+                title: data.title,
+                overview: data.overview,
+                vote_average: data.vote_average,
+                genre: data.genres[2].name,
+                release: data.release_date.split('-')[0],
+                image: {
+                    original: BASE_URL_IMAGE.original.concat(data.backdrop_path),
+                    small: BASE_URL_IMAGE.small.concat(data.backdrop_path)
+                }
+            }    
+
+            movies.push(movieData)
+
+            if (index === 0) {
+                setMainMovie(movieData)
+            }
+            
+            addMovieInList(movieData)
+            
+        })
     })
 }
 
-function loadListMovies() {
-    LIST_MOVIES.map(createMovie)
-}
-
-
-
-loadListMovies()
-setMainMovie(LIST_MOVIES[0])
+loadMovies()
